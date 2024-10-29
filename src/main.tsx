c39a7f330a1cfdb26a72fc4fa0fface8ce58249b
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-10-29 15:15:37 +0100

    feat: add chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/Components/ColorModeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider value={defaultSystem}>
+    <ColorModeProvider>
+      <App />
+    </ColorModeProvider>
+  </ChakraProvider>
 );
