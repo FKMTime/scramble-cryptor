3ce47b903b81ce9b9546d5f996514e55ae09d40a
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-01-17 22:20:40 +0100

    fix: vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,43 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths"
 
+const workerImportMetaUrlRE =
+  /\bnew\s+(?:Worker|SharedWorker)\s*\(\s*(new\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*\))/g;
+
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
+  worker: {
+    format: "es",
+    //@ts-ignore
+    plugins: [
+      {
+        name: "foo",
+        enforce: "pre",
+        transform(code) {
+          if (
+            code.includes("new Worker") &&
+            code.includes("new URL") &&
+            code.includes("import.meta.url")
+          ) {
+            const result = code.replace(
+              workerImportMetaUrlRE,
+              `((() => { throw new Error('Nested workers are disabled') })()`,
+            );
+            return result;
+          }
+        },
+      },
+    ],
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/worker/[name]-[hash].js",
+        assetFileNames: "assets/worker/[name]-[hash].js",
+      },
+    },
+  },
   plugins: [react(), tsconfigPaths()],
   clearScreen: false,
   server: {
@@ -24,4 +56,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+});
